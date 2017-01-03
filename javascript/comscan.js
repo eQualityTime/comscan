1c343bf9df51454d2c0d3b9ee7f9e8a6c9225a61
Joe Reddington <joe@joereddington.com>
2017-01-03 09:23:35 +0000

    More refactoring (added a Menu Item class), and back to six tests passing

--- javascript/comscan.js
+++ javascript/comscan.js
@@ -1,49 +1,51 @@
-
 window.comscan = window.comscan || {};
 
-
-function Board(inGraph,inutterances) { 
-//This is a spider that walks thought the pages digraph
-this.node=0//default
-this.childIndex=0;
-this.graph=inGraph;
-this.utterances=inutterances;
-this.getCurrentNode=function(){return this.graph[this.node]}
-this.getHighlight= function(){
-return this.getCurrentNode()[this.childIndex];
-};
-
-
-this.refreshHTML=function(){
-	listtable= document.getElementById('listtable');
-	for (child=0;child<this.getCurrentNode().length;child++)
-	{
-		listtable.innerHTML+="<tr><td>"+this.getCurrentNode()[child]+"</tr></td>";
-	}
-}
-
-this.move=function(){
-	this.childIndex++;
-	if (this.childIndex==this.getCurrentNode().length){
-		this.childIndex=0;
-	}	
-say(this.utterances[this.getCurrentNode()[this.childIndex]]);
-//needs to speak here as well. 
-}
-
-this.activate=function(){
-///here
-
-
+function say(message) {
+    //alert("say what you want!")//stup
 
 }
 
-} 
+function Board(inGraph, links) {
+    //This is a spider that walks thought the pages digraph
+    this.node = 0 //default
+    this.childIndex = 0;
+    this.graph = inGraph;
+    this.getCurrentNode = function() {
+        return this.graph[this.node]
+    }
+    this.getHighlight = function() {
+        return this.getCurrentNode()[this.childIndex].label;
+    };
 
 
-function say(message){
-//alert("say what you want!")//stup
+    this.refreshHTML = function() {
+        listtable = document.getElementById('listtable');
+        for (child = 0; child < this.getCurrentNode().length; child++) {
+            listtable.innerHTML += "<tr><td>" + this.getCurrentNode()[child].label + "</tr></td>";
+        }
+    }
+
+    this.move = function() {
+        this.childIndex++;
+        if (this.childIndex == this.getCurrentNode().length) {
+            this.childIndex = 0;
+        }
+        say(this.getCurrentNode()[this.childIndex].utterance);
+        //needs to speak here as well. 
+    }
+
+    this.activate = function() {
+		
+
+
+    }
+
+}//end Board class
+
+
+function MenuItem(inlabel, inlink, inUtterance){
+this.label=inlabel
+this.link=inlink
+this.utterance=inUtterance||inlabel
 
 }
-
-
--- tests/comscan.test.js
+++ tests/comscan.test.js
@@ -1,93 +1,95 @@
 /*
  * Unit tests for javascript/comscan.js
  */
-
 describe('Comscan', function() {
 
-  // inject the HTML fixture for the tests
-  beforeEach(function() {
-    var fixture = '<table id=listtable></table>';
-    document.body.insertAdjacentHTML(
-      'afterbegin', 
-      fixture);
-  });
+    // inject the HTML fixture for the tests
+    beforeEach(function() {
+        var fixture = '<table id=listtable></table>';
+        document.body.insertAdjacentHTML(
+            'afterbegin',
+            fixture);
+    });
 
 
-  // call the init function of calculator to register DOM elements
+    // call the init function of calculator to register DOM elements
 
-  it('The board, loaded with test values, shoudl have an initial highlight value of 4', function() {
-	board=testBoardA();
-	expect(board.getHighlight()).toBe(4);
-  });
+    it('The board, loaded with test values, shoudl have an initial highlight value of 4', function() {
+        board = testBoardA();
+        expect(board.getHighlight()).toBe(4);
+    });
 
 
-it('The html table has the number 43 in it', function(){
-	board=testBoardA();
-	board.refreshHTML();
-	hope= document.getElementById('listtable')
-	expect(hope.innerHTML.includes("43")).toBe(true)
+    it('The html table has the number 43 in it', function() {
+        board = testBoardA();
+        board.refreshHTML();
+        hope = document.getElementById('listtable')
+        expect(hope.innerHTML.includes("43")).toBe(true)
+    });
+
+    it('The highlight can be moved twice and the value there is 43', function() {
+        board = testBoardA();
+        board.move()
+        board.move()
+        hope = document.getElementById('listtable')
+        expect(board.getHighlight()).toBe(43)
+    });
+
+
+    it('The highlight can be moved six and the value there is 43 because it wraps around', function() {
+        board = testBoardA();
+        board.move()
+        board.move()
+        board.move()
+        board.move()
+        board.move()
+        board.move()
+        hope = document.getElementById('listtable')
+        expect(board.getHighlight()).toBe(43)
+    });
+
+
+
+    it('the highlight calls a speech function each time it is moved', function() {
+        board = testBoardA();
+        spyOn(window, "say");
+        board.move()
+        expect(say).toHaveBeenCalled();
+        //learned this at http://www.htmlgoodies.com/html5/javascript/spy-on-javascript-methods-using-the-jasmine-testing-framework.html#fbid=KJtVgELupgs
+    });
+
+
+
+    it('calls the speech function with the right argument each time it is moved', function() {
+        board = testBoardA();
+        spyOn(window, "say");
+        board.move()
+        expect(say).toHaveBeenCalledWith("Happy");
+    });
+
+
+    it('activating the third button takes us to a new page starting with 99', function() {
+        board = testBoardA();
+        board.move()
+        board.move()
+        board.activate()
+        expect(board.getHighlight()).toBe(99)
+    });
+
+    //here
+
+
+
 });
 
-it('The highlight can be moved twice and the value there is 43', function(){
-	board=testBoardA();
-	board.move()
-	board.move()
-	hope= document.getElementById('listtable')
-	expect(board.getHighlight()).toBe(43)
-});
+function testBoardA() {
 
+    pagesGraph = {
+        0: [new MenuItem(4,"", "Bashful"), new MenuItem(5,"", "Happy"), new MenuItem(43,"A", "Animals"), new MenuItem(8,"", "Frederick")],
+        "A": [new MenuItem(99,"", "Apple"), new MenuItem(2,"", "Ant"), new MenuItem(6,"", "Answer"), new MenuItem(-1,"", "")]
+    }
 
-it('The highlight can be moved six and the value there is 43 because it wraps around', function(){
-	board=testBoardA();
-	board.move()
-	board.move()
-	board.move()
-	board.move()
-	board.move()
-	board.move()
-	hope= document.getElementById('listtable')
-	expect(board.getHighlight()).toBe(43)
-});
-
-
-
-it('the highlight calls a speech function each time it is moved', function(){
-	board=testBoardA();
-	spyOn(window, "say");
-	board.move()
-	expect(say).toHaveBeenCalled();
-//learned this at http://www.htmlgoodies.com/html5/javascript/spy-on-javascript-methods-using-the-jasmine-testing-framework.html#fbid=KJtVgELupgs
-});
-
-
-
-it('calls the speech function with the right argument each time it is moved', function(){
-	board=testBoardA();
-	spyOn(window, "say");
-	board.move()
-	expect(say).toHaveBeenCalledWith("Happy");
-});
-
-
-it('activating the third button takes us to a new page starting with 99', function(){
-	board=testBoardA();
-	board.move()
-	board.move()
-	board.activate()
-	expect(board.getHighlight()).toBe(99)
-});
-
-//here
-
-
-
-});
-
-function testBoardA()
-{
-pagesGraph={0: [4,5,43,8], 43: [99,2,4,-1]}
-
-return new Board(pagesGraph,{"4":"Bashful", "5": "Happy", "43": "Animals", "8": "Frederick"});
+    return new Board(pagesGraph);
 
 
 }
